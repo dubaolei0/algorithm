7e11dc955fc97ded47e1f3a1f833883f768fb521
dubaolei <dubaolei@jkcrm.cn>
2022-06-07 15:18:58 +0800

    两数之和

--- src/main/java/algorithm/twoSum.java
+++ src/main/java/algorithm/twoSum.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -18,27 +19,53 @@ public class twoSum {
     public static void main(String[] args) {
         int target = 13;
         int[] nums = new int[]{2, 7, 11, 15};
-        int[] ints = twoSum(nums, target);
-        System.out.println(ints[0] + "分割" + ints[1]);
+        int[] ints = twoSumTwo(nums, target);
+        System.out.println("数组下标1："+ints[0] + "分数组下标2" + ints[1]);
     }
 
     /**
+     * 方法1：hash表的形式
      * map中key存放的是数组值，value是数组值对应的数组下标
+     * 数组中的元素最多遍历一次，时间复杂度为 O(n)  , 空间复杂度为O(n)，空间主要是哈希表的开销
      *
      * @param nums   数组
-     * @param target 和
+     * @param target 两数之和
      * @return
      */
     public static int[] twoSum(int[] nums, int target) {
         ConcurrentHashMap<Integer, Integer> concurrentHashMap = new ConcurrentHashMap();
         for (int i = 0; i < nums.length; i++) {
             if (concurrentHashMap.containsKey(target - nums[i])) {
-                return new int[]{i, concurrentHashMap.get(nums[i])};
+                return new int[]{i, concurrentHashMap.get(target - nums[i])};
             }
             concurrentHashMap.put(nums[i], i);
         }
         throw new IllegalArgumentException("参数异常");
     }
 
-
+    /**
+     * 这个是双指针的解法，一个指针i在数组的最前面，一个指针j在数组的最后面。双指针得提前排序一下 : Arrays.sort(array)
+     * 如果两个指针之和相等了返回
+     * 如果两个指针之和小于target，就把指针i向右移动，因为这样指针i的元素值就可以变大嘛
+     * 如果两个指针之和大于target，就把指针j向左移动，因为这样指针j的元素值就可以变小
+     * 数组中的元素最多遍历一次，时间复杂度为 O(N)。只使用了两个额外变量，空间复杂度为 O(1)
+     * @param nums  数组
+     * @param target  两数之和
+     * @return
+     */
+    public static int[] twoSumTwo(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        Arrays.sort(nums);
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+                return new int[]{i, j};
+            } else if (nums[i] + nums[j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        throw new IllegalArgumentException("参数异常");
+    }
 }
